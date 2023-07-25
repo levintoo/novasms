8821803dc044f9e11f4c4f1954aeff886a4e5495
levintoo <tooklevin@gmail.com>
2023-07-25 16:37:35 +0300

    insert dynamic fields to cursor position

--- resources/js/Components/SideBarLink.vue
+++ resources/js/Components/SideBarLink.vue
@@ -3,8 +3,8 @@
         <span v-if="active" aria-hidden="true"
               class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"></span>
         <Link
-            :class="active ? 'text-gray-800' : 'text-gray-500' "
-            class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800"
+            :class="active ? 'text-gray-800 fill-gray-800' : 'text-gray-500' "
+            class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 fill-gray-500 hover:fill-gray-800 "
             :href="href"
         >
             <slot />
