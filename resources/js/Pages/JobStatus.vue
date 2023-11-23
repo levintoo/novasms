d4938ce78421d1a20c20b4a5be4663a31d955dd2
levintoo <tooklevin@gmail.com>
2023-11-23 20:36:43 +0300

    poll job status

--- resources/js/Pages/JobStatus.vue
+++ resources/js/Pages/JobStatus.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {Head} from '@inertiajs/vue3';
+import {Head, router} from '@inertiajs/vue3';
 import AppLayout from "@/Layouts/AppLayout.vue";
 import {defineOptions, onMounted, onUnmounted} from "vue";
 import Pagination from "@/Components/Pagination.vue";
@@ -24,7 +24,7 @@ defineProps({
 })
 let update_interval
 onMounted(() => {
-    update_interval = setInterval(update,1000)
+    update_interval = setInterval(update,3000)
     console.log(update_interval)
 })
 
@@ -33,7 +33,7 @@ onUnmounted(() => {
 })
 
 const update = () => {
-    console.log("ping")
+    router.visit('/jobs')
 }
 
 window.onblur = function () {
@@ -62,7 +62,7 @@ window.onfocus = function () {
                 <TableHeadItem  class="cursor-pointer" field="name"/>
                 <TableHeadItem  class="cursor-pointer" field="progress"/>
                 <TableHeadItem  class="cursor-pointer" field="finished"/>
-                <TableHeadItem field="Actions" class="text-center"/>
+<!--                <TableHeadItem field="Actions" class="text-center"/>-->
             </TableHead>
         </template>
         <template #tbody>
@@ -75,25 +75,28 @@ window.onfocus = function () {
                         {{ `${job.info.progress}%` ?? '-' }}
                     </TableData>
                     <TableData >
-                        <span v-if="!job.info.cancelledAt" class="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full">
+                        <span v-if="!job.info.cancelledAt && !job.info.finishedAt" class="bg-gray-100 text-gray-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full">
+                           {{ !!job.info.finishedAt ?? '-' }}
+                        </span>
+                        <span v-else-if="!job.info.cancelledAt" class="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full">
                            {{ !!job.info.finishedAt ?? '-' }}
                         </span>
                         <span v-else class="bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full">
                              {{ !!job.info.finishedAt ?? '-' }}
                         </span>
                     </TableData>
-                    <td>
-                        <IconButton v-if="job.info.cancelledAt" @click="alert('dadfs')"
-                                    class="text-purple-500 focus:ring-purple-300">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
-                            </svg>
+<!--                    <td>-->
+<!--                        <IconButton v-if="job.info.cancelledAt" @click="alert('dadfs')"-->
+<!--                                    class="text-purple-500 focus:ring-purple-300">-->
+<!--                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">-->
+<!--                                <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />-->
+<!--                            </svg>-->
 
-                        </IconButton>
-                        <span v-else>
-                            -
-                        </span>
-                    </td>
+<!--                        </IconButton>-->
+<!--                        <span v-else>-->
+<!--                            - -->
+<!--                        </span>-->
+<!--                    </td>-->
                 </TableBodyItem>
             </TableBody>
 
