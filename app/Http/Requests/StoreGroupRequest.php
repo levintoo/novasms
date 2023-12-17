7259f195116a70fd40ccbf46c6aa252dc3053b9c
levintoo <tooklevin@gmail.com>
2023-12-17 17:52:18 +0300

    add group validation in form requests

--- app/Http/Requests/StoreGroupRequest.php
+++ app/Http/Requests/StoreGroupRequest.php
@@ -7,14 +7,6 @@ use Illuminate\Foundation\Http\FormRequest;
 class StoreGroupRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
@@ -22,7 +14,8 @@ class StoreGroupRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required','string','max:255'],
+            'description' => ['nullable','string','max:2550'],
         ];
     }
 }
