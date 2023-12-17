438d45e3685b66ae3cf492c8009eabc189ebcdb8
levintoo <tooklevin@gmail.com>
2023-12-17 20:02:24 +0300

    Group controller routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\GroupController;
 use App\Http\Controllers\MessageController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -53,6 +55,54 @@ Route::middleware(['auth', 'verified'])->group(function () {
         Route::post('send', 'send')->name('send');
 
     });
+
+    Route::controller(GroupController::class)
+
+        ->prefix('group')
+
+        ->name('group.')
+
+        ->group(function () {
+
+            Route::get('/', 'index')->name('index');
+
+            Route::get('create', 'create')->name('create');
+
+            Route::post('create', 'store')->name('store');
+
+            Route::get('edit/{group}', 'edit')->name('edit');
+
+            Route::patch('edit/{group}', 'update')->name('update');
+
+            Route::delete('{group}', 'destroy')->name('delete');
+
+    });
+
+    Route::controller(ContactController::class)
+
+        ->prefix('contact')
+
+        ->name('contact.')
+
+        ->group(function () {
+
+            Route::get('/', 'index')->name('index');
+
+            Route::get('create', 'InputCreate')->name('input.create');
+
+            Route::post('create', 'inputStore')->name('input.store');
+
+            Route::get('upload', 'uploadCreate')->name('upload.create');
+
+            Route::post('upload', 'UploadStore')->name('upload.store');
+
+            Route::get('edit/{contact}', 'edit')->name('edit');
+
+            Route::patch('edit/{contact}', 'update')->name('update');
+
+            Route::delete('{contact}', 'destroy')->name('delete');
+
+        });
 
 });
 
