cd5e15f8ec4c3b029e20d0db4b989fbf2dad7094
levintoo <tooklevin@gmail.com>
2023-12-17 16:21:44 +0300

    add table components

---
+++ resources/js/Components/Table.vue
@@ -0,0 +1,12 @@
+<template>
+    <!-- New Table -->
+    <div class="w-full overflow-hidden rounded-lg shadow-xs">
+        <div class="w-full overflow-x-auto">
+            <table class="w-full whitespace-no-wrap">
+                <slot name="thead"></slot>
+                <slot name="tbody"></slot>
+            </table>
+        </div>
+        <slot name="pagination"></slot>
+    </div>
+</template>
