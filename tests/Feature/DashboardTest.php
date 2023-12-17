d81ba2dcd871a274d33176d5d94cb956acdb56c1
levintoo <tooklevin@gmail.com>
2023-12-17 16:23:44 +0300

    add a dashboard test

---
+++ tests/Feature/DashboardTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class DashboardTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_dashboard_is_displayed()
+    {
+        $user = User::factory()->create();
+
+        $response = $this
+            ->actingAs($user)
+            ->get('/dashboard');
+
+        $response->assertOk();
+    }
+
+    public function test_dashboard_is_not_accessed_without_login()
+    {
+        $response = $this->get('/dashboard');
+
+        $response->assertStatus(302);
+    }
+}
