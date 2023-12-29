5cb0459cfd88d852c91847e8ea5ae0b6f5200f2d
levintoo <tooklevin@gmail.com>
2023-12-29 16:42:33 +0300

    allow only super admins edit super admin roles

--- app/Http/Requests/Admin/UpdateUserRequest.php
+++ app/Http/Requests/Admin/UpdateUserRequest.php
@@ -2,11 +2,45 @@
 
 namespace App\Http\Requests\Admin;
 
+use App\Models\User;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
 class UpdateUserRequest extends FormRequest
 {
+    /**
+     * * Determine if the user is authorized to make this request.
+     * */
+    public function authorize(): bool
+    {
+
+        $user = User::query()
+
+            ->select('email','id')
+
+            ->where('email',$this->email)
+
+            ->firstOrFail();
+
+        $currentRoles = $user->getRoleNames();
+
+        $admin = \Auth::user();
+
+        if ($admin->email === $this->email) {
+            return false;
+        }
+        elseif ($admin->hasRole('super admin')) {
+            return true;
+        }
+        elseif ($currentRoles->doesntContain('super admin') && $this->role !== 'super admin') {
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+
     /**
      * Get the validation rules that apply to the request.
      *
@@ -16,7 +50,9 @@ class UpdateUserRequest extends FormRequest
     {
         return [
             'name' => ['required','max:255','string'],
+
             'email' => ['required','max:255','string'],
+
             'role' => ['required', Rule::exists('roles','name')],
         ];
     }
