6b379eeb99c75086583eb38c491f78073755c902
levintoo <tooklevin@gmail.com>
2024-01-29 18:00:06 +0300

    add sms rate config

---
+++ app/Http/Controllers/Api/PaystackWebhookController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Enums\TransactionStatus;
+use App\Http\Controllers\Controller;
+use App\Models\Account;
+use App\Models\Transaction;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class PaystackWebhookController extends Controller
+{
+    public function __invoke (Request $request) {
+        $whiteListedIps = collect([
+            '52.31.139.75',
+            '52.49.173.169',
+            '52.214.14.220',
+        ]);
+
+//        $ip = $request->getClientIp();
+//
+//        if (!$whiteListedIps->contains($ip)) {
+//            info('ip('.$ip.') is not whitelisted for paystack webhooks');
+//            return;
+//        }
+
+        if (!$request->hasHeader('X-Paystack-Signature')) {
+            abort(400);
+        }
+
+        info($request->header('X-Paystack-Signature'));
+
+        $signature = $request->header('X-Paystack-Signature');
+
+        $secretKey = config('app.paystack_secret_key');
+
+        if($signature !== hash_hmac('sha512', $request->getContent(), $secretKey)) {
+            info('failed hash test');
+        }
+
+        if ($request->event === 'charge.success') {
+            $this->handleChargeSuccess($request);
+        }
+    }
+
+    public function handleChargeSuccess($request) {
+        $transaction = Transaction::where('transaction_id', $request['data']['reference'])->firstOrFail();
+
+        $account = Account::where('user_id', $transaction->user_id)->firstorfail();
+
+        $smsRate = config('app.sms_rate');
+
+        $amount = $request['data']['amount'] / 100;
+
+        $sms = $amount / $smsRate;
+
+        $account->balance = $account->balance + $sms;
+
+        $transaction->status = TransactionStatus::COMPLETED;
+
+        DB::transaction(function () use ($request, $account, $transaction) {
+            $account->save();
+            $transaction->save();
+        });
+    }
+}
--- config/app.php
+++ config/app.php
@@ -189,6 +189,8 @@ return [
 
     'sms_sender_id' => env('MOBILESASA_SENDER_ID'),
 
+    'sms_rate' => env('SMS_RATE', 1),
+
     'date_time_format' => 'd-m-Y H:i:s T',
 
     'paystack_secret_key' => env('PAYSTACK_SECRET_KEY'),
