8865471b1adb62dac5445ae89af47ceb209675bf
levintoo <tooklevin@gmail.com>
2024-02-01 09:56:41 +0300

    bug fix: display appropriate colors in account history status

--- resources/js/Pages/Account/Index.vue
+++ resources/js/Pages/Account/Index.vue
@@ -165,7 +165,7 @@ watch(params.value, debounce((params) => {
                                 {{ transaction.amount ?? '-' }}
                             </TableData>
                             <TableData>
-                                <span v-if="transaction.status === 'finished' "
+                                <span v-if="transaction.status === 'completed' "
                                       class="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full">
                                     {{ transaction.status ?? '-' }}
                                 </span>
