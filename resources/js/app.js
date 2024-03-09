52f41deceb53a3f78d832a1e237b92df24132d4f
levintoo <tooklevin@gmail.com>
2024-03-09 18:30:11 +0300

    rename app defaults to flairsms

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,7 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Novasms';
+const appName = import.meta.env.VITE_APP_NAME || 'Flairsms';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
